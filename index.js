158f6569ff7da6e95a5a093a4c055a376329c0bb
sachinsisondiya <Sachinsisondiya912@gmail.com>
2025-08-15 23:18:16 +0530

    adding api

--- index.js
+++ index.js
@@ -1,11 +1,31 @@
+const specialDish = document.getElementById("special");
 
-
-
-
-fetch("https://www.themealdb.com/api/json/v1/1/search.php?s=pasta")
+fetch("https://www.themealdb.com/api/json/v1/1/random.php")
   .then(res => res.json())
   .then(data => {
-    console.log(data.meals[0].strYoutube)
+    console.log(data)
+    const meal = data.meals[0];
+    specialDish.innerHTML += `
+      <img src="${meal.strMealThumb}" alt="${meal.strMeal}" />
+      <h3 class="dish-name">${meal.strMeal}</h3>
+      <p class="type">${meal.strCategory}</p>
+      <p class="origin">${meal.strArea}</p>
+      <div class="ingredients">
+        <h4>Ingredients:</h4>
+        <ul class="ingredient-list">
+          ${Object.keys(meal)
+            .filter(key => key.startsWith("strIngredient") && meal[key])
+            .map(key => `<li>${meal[key]}</li>`)
+            .join("")}
+        </ul>
+      </div>
+      <div class="process">
+        <h4>Cooking Instructions:</h4>
+        <p>${meal.strInstructions}</p>
+      </div>
+    `;
+
+    // console.log(data.meals[0].strYoutube)
   })
   .catch("not allowed")
 // fetch('https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/random?tags=vegetarian%2Cdessert&number=1', {

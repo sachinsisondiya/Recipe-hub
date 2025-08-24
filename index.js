7c6e2a5edd81fd18fa30eb5bf00e3964b19d3c18
sachinsisondiya <Sachinsisondiya912@gmail.com>
2025-08-24 23:14:29 +0530

    search bar complete

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const specialDish = document.getElementById("special");
 const searchBar = document.getElementById("search-bar")
 const menuBar = document.getElementById("menu-icon")
 const searchInput = document.getElementById("search-input")
+const searchResult = document.getElementById("search-result")
 
 fetch("https://www.themealdb.com/api/json/v1/1/random.php")
   .then(res => res.json())
@@ -57,17 +58,58 @@ function search(){
  
   searchBar.style.display = "block"
   menuBar.style.display = "none"
-  console.log(searchInput)
   searchInput.addEventListener("input" , (e)=>{
       let value = e.target.value
-      console.log(value)
       fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${value}`)
       .then(res => res.json())
       .then(data =>{
-        console.log(data)
+        const meal = data.meals
+        console.log(meal)
+        if(meal){
+          searchResult.innerHTML = meal.slice(0,5).map( m =>{
+            return `<p onclick="selectMeal('${m.idMeal}')">${m.strMeal}</p>`
+          }).join("")
+        }
       })
   })
   
+}
+function selectMeal(id){
+  fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${id}`)
+  .then(res => res.json())
+  .then(data =>{
+    const meal = data.meals[0]
+     const ingredients = [];
+    console.log(meal.strIngredient1);
+    for(let i =1;i<=10;i++){
+      let ingredient  = meal[`strIngredient${i}`];
+      ingredients.push(ingredient);
+
+    }
+    specialDish.innerHTML = `
+      <img src="${meal.strMealThumb}" alt="${meal.strMeal}" class="cover-image" />
+      <div class="blur"></div>
+      <div class="spec-intro">
+      <h3 class="dish-name"> Name:${meal.strMeal}</h3>
+      <p class="type">Category: ${meal.strCategory}</p>
+      <p class="origin">Origin: ${meal.strArea}</p>
+      </div>
+      <div class="meal-info">
+      <div class="ingredients">
+        <h4>Ingredients:</h4>
+        <div class="ingredient-list">
+          ${ingredients.map(i =>{
+            return `<span>${i}</span>`
+          }).join(",")}
+        </div>
+      </div>
+      </div>
+          <div class="process">
+         <h4>Cooking Instructions:</h4>
+         <p>${meal.strInstructions}</p>
+       </div>
+       `
+  })
 }
 
 // fetch('https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/random?tags=vegetarian%2Cdessert&number=1', {
